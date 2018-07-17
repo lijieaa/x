8c3c85c96ecc25a26de47e4f160b30f4ca1d093f
jayleehtt <952054767@qq.com>
2018-07-17 15:39:28 +0800

    wol

--- TcpServer.py
+++ TcpServer.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
 import logging
 #from tornado.ioloop import IOLoop
 from tornado import ioloop
@@ -8,6 +10,13 @@ from tornado.tcpserver import TCPServer
 from tornado.options import options, define
 import os
 
+import socket
+import struct
+
+BROADCAST_IP = '192.168.30.255'
+DEFAULT_PORT = 9
+
+
 define("port", default=9888, help="TCP port to listen on")
 logger = logging.getLogger(__name__)
 
@@ -15,8 +24,71 @@ logger = logging.getLogger(__name__)
 #打开串口
 serialPort="COM1"   #串口
 baudRate=9600       #波特率
-ser=serial.Serial(serialPort,baudRate,timeout=0.5)
+ser1=serial.Serial("COM1",baudRate,timeout=0.5)
+ser2=serial.Serial("COM2",baudRate,timeout=0.5)
+ser3=serial.Serial("COM3",baudRate,timeout=0.5)
+
 print("参数设置：串口=%s ，波特率=%d"%(serialPort,baudRate))
+
+
+def create_magic_packet(macaddress):
+    """
+    Create a magic packet which can be used for wake on lan using the
+    mac address given as a parameter.
+
+    Keyword arguments:
+    :arg macaddress: the mac address that should be parsed into a magic
+                     packet.
+
+    """
+    if len(macaddress) == 12:
+        pass
+    elif len(macaddress) == 17:
+        sep = macaddress[2]
+        macaddress = macaddress.replace(sep, '')
+    else:
+        raise ValueError('Incorrect MAC address format')
+
+    # Pad the synchronization stream
+    data = b'FFFFFFFFFFFF' + (macaddress * 20).encode()
+    send_data = b''
+
+    # Split up the hex values in pack
+    for i in range(0, len(data), 2):
+        send_data += struct.pack(b'B', int(data[i: i + 2], 16))
+    return send_data
+
+
+def send_magic_packet(*macs, **kwargs):
+    """
+    Wakes the computer with the given mac address if wake on lan is
+    enabled on that host.
+
+    Keyword arguments:
+    :arguments macs: One or more macaddresses of machines to wake.
+    :key ip_address: the ip address of the host to send the magic packet
+                     to (default "255.255.255.255")
+    :key port: the port of the host to send the magic packet to
+               (default 9)
+
+    """
+    packets = []
+    ip = kwargs.pop('ip_address', BROADCAST_IP)
+    port = kwargs.pop('port', DEFAULT_PORT)
+    for k in kwargs:
+        raise TypeError('send_magic_packet() got an unexpected keyword '
+                        'argument {!r}'.format(k))
+
+    for mac in macs:
+        packet = create_magic_packet(mac)
+        packets.append(packet)
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sock.connect((ip, port))
+    for packet in packets:
+        sock.send(packet)
+    sock.close()
 
 
 class EchoServer(TCPServer):
@@ -28,6 +100,17 @@ class EchoServer(TCPServer):
             try:
                 data = yield stream.read_until(b"\n")
                 logger.info("Received bytes: %s", data)
+                temp = str(data, encoding="utf-8").strip()
+                #print(temp)
+                if temp is '1':
+                    print("on")
+                    send_magic_packet('ff.ff.ff.ff.ff.ff', '08-60-6E-75-98-2D', 'F8-B1-56-B5-06-D5',
+                                      '10-7B-44-92-F1-B9', 'FFFFFFFFFFFF')
+                    ser1.write("1".encode())
+                    ser2.write("2".encode())
+                    ser3.write("3".encode())
+                else:
+                    print("off")
                 if not data.endswith(b"\n"):
                     data = data + b"\n"
                 yield stream.write(data)
@@ -39,8 +122,10 @@ class EchoServer(TCPServer):
 
     def ack(self):
         print('ack')
-        len  = ser.write("6456456464".encode())
-        print(len)  # 可以接收中文
+        #len1  = ser1.write("6456456464".encode())
+        #len2  = ser2.write("6456456464".encode())
+        #len3  = ser3.write("6456456464".encode())
+        #print(len1)  # 可以接收中文
         status = os.system("ping 192.168.5.22299")
         #print('-----:'+status)
         for c in self.clients:
